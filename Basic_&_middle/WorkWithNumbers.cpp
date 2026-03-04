bdc9a365118c3f240fb73cd6c5abbd2e7fdf77b7
Pedro Eduardo Medina <83928073+p-eduardo-medina@users.noreply.github.com>
2026-03-03 18:18:41 -0600

    Commit on 03-03-2026

---
+++ Basic_&_middle/WorkWithNumbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+
+    double num3 = 5.5;
+    double num4 = 2.3;
+    double product = num3 * num4;
+    cout << "The product of " << num3 << " and " << num4 << " is: " << product << endl;
+
+    return 0;
+}
