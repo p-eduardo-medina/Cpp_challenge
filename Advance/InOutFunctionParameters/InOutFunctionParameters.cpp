44a551d3cf4b5a179e9d5f2821d52e5255c17ba6
Pedro Eduardo Medina <83928073+p-eduardo-medina@users.noreply.github.com>
2026-04-28 00:37:57 -0600

    InOut Function Parameters

---
+++ Advance/InOutFunctionParameters/InOutFunctionParameters.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+void max_str(const std::string& input1, const std::string& input2, 
+                    std::string& output) {
+    if (input1.length() > input2.length()) {
+        output = input1;
+    } else {
+        output = input2;
+    }
+}
+
+void max_int(int input1, int input2, int& output) {
+    if (input1 > input2) {
+        output = input1;
+    } else {
+        output = input2;
+    }
+}
+
+int main() {
+    
+    std::string output;
+    std::string input1 = "Hello World";
+    std::string input2 = "World!!!";
+    max_str(input1, input2, output);
+    std::cout << "The longer string is: " << output << "\n\n" << std::endl;
+
+    int out_int;
+    int in1{100};
+    int in2{20};
+    max_int(in1, in2, out_int);
+    std::cout << "The larger integer is: " << out_int << "\n\n" << std::endl;
+
+
+    return 0;
+
+}
